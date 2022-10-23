3ec059779cc5a7a6b410f07a5949fdcb90a9487c
dorklein <dorklein2@gmail.com>
2022-10-23 14:55:39 +0300

    CI: fixed CI test

--- scripts/build.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash -e
-cd "$( dirname "${BASH_SOURCE[0]}" )"
-cd ..
-
-rm -r dist/
-python -m build
--- scripts/upload.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash -e
-cd "$( dirname "${BASH_SOURCE[0]}" )"
-cd ..
-
-twine upload dist/*
