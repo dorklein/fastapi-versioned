9dac03c3fb92749eb2dd5dd380929329da595bb6
dorklein <dorklein2@gmail.com>
2022-10-23 12:20:22 +0300

    CI: creating ci env

--- scripts/bumpversion.sh
+++ scripts/bumpversion.sh
@@ -2,9 +2,13 @@
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 cd ..
 
+VERSION_RULE=${1:?"Usage: ./scripts/bumpversion.sh <version rule>"}
+
 git fetch origin
 git checkout -B master
 git reset --soft origin/master
-bumpversion "$@"
+POETRY_OUTPUT=$(poetry version "$VERSION_RULE" 2>&1)
+NEW_VERSION=$( perl -ne 'if (/Bumping version from \d+\.\d+\.\d+ to (\d+\.\d+\.\d+)/) { print $1 . "\n" }' <<< "$POETRY_OUTPUT" )
+git tag -a "v$NEW_VERSION" -m "Bumped version to $NEW_VERSION"
 git push
 git push --tags
