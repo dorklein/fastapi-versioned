5b735947ccc1b79a4084a5156e30da6a8d4ec0fd
dorklein <dorklein2@gmail.com>
2022-10-23 14:38:46 +0300

    CI: creating bumpversion script

--- scripts/bumpversion.sh
+++ scripts/bumpversion.sh
@@ -9,6 +9,8 @@ git checkout -B master
 git reset --soft origin/master
 POETRY_OUTPUT=$(poetry version "$VERSION_RULE" 2>&1)
 NEW_VERSION=$( perl -ne 'if (/Bumping version from \d+\.\d+\.\d+ to (\d+\.\d+\.\d+)/) { print $1 . "\n" }' <<< "$POETRY_OUTPUT" )
+git commit ./pyproject.toml -m "Bumped version to $NEW_VERSION"
 git tag -a "v$NEW_VERSION" -m "Bumped version to $NEW_VERSION"
 git push
 git push --tags
+gh release create "v$NEW_VERSION" --generate-notes
